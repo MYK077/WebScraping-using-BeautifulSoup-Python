2bd001144538b1d0139e6a7502e3897cbaf32031
Mayank <myk.vit@gmail.com>
2019-05-17 20:02:41 -0600

    picking data from number of pages set in the code

--- app.py
+++ app.py
@@ -3,10 +3,11 @@
   #2: collect data from each detail page
   #3: collect all links to detail pages  of each product
   #4: write scraped data to a csv file
-
 # import request so that you can use get() method to get the url
+
 from bs4 import BeautifulSoup
 import requests
+
 
 def get_page_url(url):
     # requests.get url takes html data from the web page, .text will make in as text
@@ -15,20 +16,65 @@ def get_page_url(url):
     if not response:
         print("server responded:",response.status_code)
     else:
-        # this parse the html data captured by requests.get(url) into xml as we are using lxml as parser
-        soup = BeautifulSoup(response,'lxml')
-        title = soup.find('h1',id='itemTitle').get_text(strip= True)
-        print(title)
-        # strip() removes space at the begining and end of the string and split splits the string at space
-        price = soup.find('span',id='prcIsum').text.strip().split(' ')
-        print(price)
+        try:
+            # this parse the html data captured by requests.get(url) into xml as we are using lxml as parser
+            soup = BeautifulSoup(response,'lxml')
+            detail_url = soup.findAll('a',class_='s-item__link')
+            list_urls = []
+            for url in detail_url:
+# soup.findAll returns all the achor tages with class_= s-item__link but we want to pull only href and store them in
+# an empty lists
+                list_urls.append(url.get('href'))
+            return (list_urls)
+        except:
+            detail_url = []
 
-        itemsSold = soup.find('div',class_='w2b-cnt w2b-3 w2b-brdr').text
-        print(itemsSold)
+
+def get_details(url):
+    for item in url:
+        response = requests.get(item).text
+        if not response:
+            print("server responded:",response.status_code)
+        else:
+            soup = BeautifulSoup(response,'lxml')
+            try:
+                title = soup.find('h1',id='itemTitle').get_text(strip= True)
+                print(title)
+            except:
+                title = []
+            try:
+                # strip() removes space at the begining and end of the string and split splits the string at space
+                price = soup.find('span',id='prcIsum').text.strip().split(' ')
+                print(price)
+            except:
+                price = []
+            try:
+                itemsSold = soup.find('div',class_='w2b-cnt w2b-3 w2b-brdr').text
+                print(itemsSold)
+            except:
+                itemsSold = []
+            data ={
+            'itemTitle': title,
+            'price': price,
+            'currency': currency,
+            'total':itemsSold
+            }
+    return data
+
 
 def main():
-    url = "https://www.ebay.com/itm/New-Cool-Digital-Watch-for-Kids-Boys-Girls-Slap-on-Teen-Children-Cute-Presents/292908907519?hash=item4432bb3bff:m:m8o9L0QKf8IblKOhyw9mj5w:sc:USPSFirstClass!84102!US!-1"
-    return get_page_url(url)
+    num = 1
+    url = "https://www.ebay.com/b/Wristwatches/31387/bn_2408451?rt=nc&_pgn="
+    for num in range(1,51):
+        get_details(get_page_url(url))
+        num = num + 1
+        url = url+str(num)
+
+csv_writer = csv.writer(csv_file)
+rows = [data['title'],data['price'],data['currency'],data['total']]
+csv_writer.writerow(rows)
+    # return get_details(get_page_url(url))
+    # return get_page_url(url)
 
 
 
